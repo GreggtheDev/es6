66b526f0a0574a3cd57569f5feff3f44000d33c8
GreggtheDev <greggthedev@gmail.com>
2024-07-03 17:24:23 -0400

    Used spread operator to create an array called numbers that combines the elements of numbers1 and numbers2

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
 // Step 1: Create two arrays of numbers called numbers1 and numbers2
 const numbers1 = [1, 2, 3, 4, 5];
 const numbers2 = [6, 7, 8, 9, 10];
+
+// Step 2: Use the spread operator to create an array called numbers that combines the elements of numbers1 and numbers2
+const numbers = [...numbers1, ...numbers2];
